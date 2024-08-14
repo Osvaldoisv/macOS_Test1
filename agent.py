15cf47094ea0ca6afe8af5c4bee73b24af35a146
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-08-14 16:32:47 -0400

    remove anthropic

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ from typing import TypedDict, Annotated, Sequence, Literal
 
 from functools import lru_cache
 from langchain_core.messages import BaseMessage
-from langchain_anthropic import ChatAnthropic
+# from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import ToolNode
@@ -10,12 +10,13 @@ from langgraph.graph import StateGraph, END, add_messages
 
 tools = [TavilySearchResults(max_results=1)]
 
+
 @lru_cache(maxsize=4)
 def _get_model(model_name: str):
     if model_name == "openai":
         model = ChatOpenAI(temperature=0, model_name="gpt-4o")
     elif model_name == "anthropic":
-        model =  ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
+        model = ChatOpenAI(temperature=0, model_name="gpt-4o")
     else:
         raise ValueError(f"Unsupported model type: {model_name}")
 
@@ -49,11 +50,12 @@ system_prompt = """Tu eres un traductor de lenguaje experto y traduces todo lo q
                 4. Tu no explicas lo que haces, solo traduces y ya.
                 """
 
+
 # Define the function that calls the model
 def call_model(state, config):
     messages = state["messages"]
     messages = [{"role": "system", "content": system_prompt}] + messages
-    model_name = config.get('configurable', {}).get("model_name", "anthropic")
+    model_name = config.get("configurable", {}).get("model_name", "anthropic")
     model = _get_model(model_name)
     response = model.invoke(messages)
     # We return a list, because this will get added to the existing list
@@ -62,6 +64,7 @@ def call_model(state, config):
 
 # Define the function to execute tools
 tool_node = ToolNode(tools)
+
 
 # Define the config
 class GraphConfig(TypedDict):
